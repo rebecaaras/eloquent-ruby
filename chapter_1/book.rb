c5ba423e957b492534a5219b3b45e2301cea1815
rebecaaras <rebecaaraujo2013@gmail.com>
2025-12-18 17:52:13 -0300

    refactor: remove unnecessary comments and organize exercises within chapter folders

--- chapter_1/book.rb
+++ chapter_1/book.rb
@@ -1,12 +1,12 @@
 class Book
-  attr_accessor :title, :author, :content #does what?
+  attr_accessor :title, :author, :content
 
-  def initialize(title, author, content) #does what?
+  def initialize(title, author, content)
     @title   = title
     @author  = author
     @content = content
   end
-
+  
   def words
     @content.split
   end
