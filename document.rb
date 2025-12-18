46adc000bf43677c94d81356146db342cdadbc82
rebecaaras <rebecaaraujo2013@gmail.com>
2025-12-18 15:57:22 -0300

    add class book

---
+++ document.rb
@@ -0,0 +1,17 @@
+class Document
+  attr_accessor :title, :author, :content #does what?
+
+  def initialize(title, author, content) #does what?
+    @title   = title
+    @author  = author
+    @content = content
+  end
+
+  def words
+    @content.split
+  end
+
+  def word_count
+    words.size
+  end
+end
