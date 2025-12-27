5874bef73f975fb8f513ad175465f086a2b61d8a
rebecaaras <rebecaaraujo2013@gmail.com>
2025-12-27 19:00:58 -0300

    refactor: makes checking for readonly to set title, an oneliner

--- chapter_2/book.rb
+++ chapter_2/book.rb
@@ -2,11 +2,6 @@ class Book
   attr_accessor :read_only
 
   def title=new_title
-    unless @read_only
-      @title = new_title
-      return @title
-    end
-
-    puts "Title cannot be changed."
+    @title = new_title unless @read_only
   end
 end
